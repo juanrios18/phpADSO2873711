65006088a00c93b4ab111490468208d822873f8a
juanrios18 <juanpabloaristizabal3@gmail.com>
2024-12-03 18:11:36 -0500

    punto 1 - Guia php terminado

--- POO_php/ejercicios/ejer01/Empleado.class.php
+++ POO_php/ejercicios/ejer01/Empleado.class.php
@@ -1,44 +1,57 @@
 <?php
-    class Empleado{
-        private $nombre;
-        private $sueldo;
-        private $email;
-        private $fechaNacimiento;
-        private $peso;
-        private $altura;
+class Empleado {
+    private $nombre;
+    private $sueldo;
+    private $email;
+    private $fechaNacimiento;
+    private $peso;
+    private $altura;
 
-        public function __construct($nombre = null, $sueldo = null, $email = null, $fechaNacimiento = null, $peso = null, $altura = null) {
-            echo "<br>Se crea el objeto persona</br>";
-            $this->$nombre = $nombre;
-            $this->$sueldo = $sueldo;
-            $this->$email = $email;
-            $this->$fechaNacimiento = $fechaNacimiento;
-            $this->$peso = $peso;
-            $this->$altura = $altura;
+    public function __construct($nombre = null, $sueldo = null, $email = null, $fechaNacimiento = null, $peso = null, $altura = null) {
+        $this->nombre = $nombre;
+        $this->sueldo = $sueldo;
+        $this->email = $email;
+        $this->fechaNacimiento = $fechaNacimiento;
+        $this->peso = $peso;
+        $this->altura = $altura;
+    }
+
+    public function datosCom() {
+        echo "<div class='form-group'>";
+        echo "<div class='strong-data'>Nombre del empleado:</div> $this->nombre.";
+        echo "<div class='strong-data'>Sueldo del empleado:</div> $this->sueldo.";
+        if ($this->impuestos($this->sueldo)) {
+            echo "<div>-Debe pagar impuestos.</div>";
+        } else {
+            echo "<div>-No debe pagar impuestos.</div>";
         }
+        $this->edad($this->fechaNacimiento);
+        echo "</div>";
+    }
 
-        public function datosCom($nombre, $sueldo){
-            echo "<br> El nombre del empleado es: $nombre</br>";
-            if ($this->impuestos($this->$sueldo)) {
-                echo "<br>Debe pagar impuestos</br>";
-            }else {
-                echo "<br>No debe pagar impuestos</br>";
-            }
-        }
+    private function impuestos($sueldo) {
+        return $sueldo > 2000000;
+    }
 
-        private function impuestos($sueldo) {
-            return $sueldo > 2000000;
-        }
+    private function edad($fechaNacimiento) {
+        $anioActual = date("Y");
+        $edad = $anioActual - date("Y", strtotime($fechaNacimiento));
 
-        private function edad($fechaNacimiento) {
-            $anioActual = date("Y");
-            $edad = $anioActual - $fechaNacimiento("Y");
-
-            if ($edad >= 18) {
-                echo "<br>Puede votar</br>";
-            } else {
-                echo "<br>No puede votar</br>";
-            }
+        echo "<div class='strong-data'>Edad del empleado:</div> $edad años.";
+        if ($edad >= 18) {
+            echo "<div>-Puede votar.</div>";
+        } else {
+            echo "<div>-No puede votar.</div>";
         }
     }
+
+    public function inicializar($nombre, $sueldo, $email, $fechaNacimiento, $peso, $altura) {
+        $this->nombre = $nombre;
+        $this->sueldo = $sueldo;
+        $this->email = $email;
+        $this->fechaNacimiento = $fechaNacimiento;
+        $this->peso = $peso;
+        $this->altura = $altura;
+    }
+}
 ?>
